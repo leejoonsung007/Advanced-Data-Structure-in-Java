f5d3842a63a4309339b52798cb2495e8aa6dffa8
leejoonsung <juncheng.li@ucdconnect.ie>
2018-02-04 11:20:24 +0000

    update

--- Assignment2/src/Main.java
+++ Assignment2/src/Main.java
@@ -10,12 +10,12 @@ public class Main
         ArrayList<Car> cars = new ArrayList<Car>();
         ArrayList<Truck> trucks = new ArrayList<Truck>();
 
-        HandleBnb.readInputPropertyFile("MyBnb/src/InputProperty.txt", apartments, houses, villas);
+        HandleBnb.readInputPropertyFile("src/InputProperty.txt", apartments, houses, villas);
         HandleBnb.GiveRentalProperty(apartments, houses, villas);
-        HandleBnb.PrintAllProperties("MyBnb/src/OutputProperty.txt",apartments, houses, villas);
-        HandleVehicle.readInputVehicleFile("MyBnb/src/InputVehicle.txt", cars, trucks);
-        HandleVehicle.PrintAllVehicle("MyBnb/src/OutputVehicle.txt", cars, trucks);
-        HandleVehicle.CalculateVehicleTotalIncome(cars, trucks);
+        HandleBnb.PrintAllProperties("src/OutputProperty.txt",apartments, houses, villas);
+//        HandleVehicle.readInputVehicleFile("Assignment2/src/InputVehicle.txt", cars, trucks);
+//        HandleVehicle.PrintAllVehicle("Assignment2/src/OutputVehicle.txt", cars, trucks);
+//        HandleVehicle.CalculateVehicleTotalIncome(cars, trucks);
 
 //        HandlingBnb.CalculateTotalIncome(apartments, houses, villas);
 
